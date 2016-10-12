2d59934681f7fcabf3418526ea67bb5f13bfd052
guirad <guirado.maxime@gmail.com>
2016-10-12 21:30:18 +0200

    webApi in WebService

---
+++ Max/TestWebApi/testService/Controllers/Routed2Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OWINTest.Service.API
+{
+    [RoutePrefix("api/testing2")]
+    public class Routed2Controller : ApiController
+    {
+        [Route("v1/getall")]
+        public IEnumerable<string> GetAllItemsv1()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [Route("getall")]
+        public IEnumerable<string> GetAllItems()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Max/TestWebApi/testService/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testService.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public int? id { get; set; }
+    }
+}
