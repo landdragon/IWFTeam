a36a7abec7001a5c2cad34313eb205950806dd37
guirad <guirado.maxime@gmail.com>
2016-10-12 19:29:07 +0200

    webApi with swagger

---
+++ Max/TestWebApi/TestWebApi/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TestWebApi.Models;
+
+namespace TestWebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        static IList<Product>  listProduct = new List<Product>();
+        public IList<Product> Get()
+        {
+            return listProduct;
+        }
+
+        public bool Post(int? id, string name)
+        {
+            listProduct.Add(new Product { id = id, name = name });
+            return true;
+
+        }
+
+        public bool Put([FromUri]int id, Product productMod)
+        {
+            var product  = listProduct.First(x => x.id == id);
+            product.id = productMod.id;
+            product.name = productMod.name;
+            return true;
+        }
+        public bool Delete([FromUri]int id)
+        {
+            var product  = listProduct.First(x => x.id == id);
+            listProduct.Remove(product);
+            return true;
+        }
+    }
+}
---
+++ Max/TestWebApi/TestWebApi/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApi.Models
+{
+    public class Product
+    {
+        public string name { get; set; }
+        public int? id { get; set; }
+    }
+}
